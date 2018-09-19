06bd4546d31488fb8458cc65bbce99c07552dd4e
savkk <savkk@bk.ru>
2018-09-20 00:12:36 +0300

    hooks

--- src/test/java/com/github.savkk/stepdefs/Hooks.java
+++ src/test/java/com/github.savkk/stepdefs/Hooks.java
@@ -4,26 +4,33 @@ import cucumber.api.java.After;
 import cucumber.api.java.AfterStep;
 import cucumber.api.java.Before;
 import cucumber.api.java.BeforeStep;
+import cucumber.api.java.ru.Ктомуже;
 
 public class Hooks {
+    private static int count;
 
-    @Before
+    @Ктомуже("Первый/Второй/Третий шаг")
+    public void empty() {
+        System.out.println(++Hooks.count);
+    }
+
+    @Before(value = "(@hooks or @only_scenario_hooks) and not @only_step_hooks")
     public void before() {
-        System.out.println("before scenario");
+        System.out.println(count + " before scenario");
     }
 
-    @AfterStep()
+    @AfterStep(value = "not(@hooks or @only_scenario_hooks) and @only_step_hooks")
     public void afterStep() {
-        System.out.println("afterstep");
+        System.out.println(count + " after step");
     }
 
-    @BeforeStep
+    @BeforeStep(value = "@only_step_hooks")
     public void beforeStep() {
-        System.out.println("beforestep");
+        System.out.println(count + " before step");
     }
 
-    @After
+    @After(value = "@hooks or @only_scenario_hooks")
     public void after() {
-        System.out.println("after scenarion");
+        System.out.println(count + " after scenario");
     }
 }
